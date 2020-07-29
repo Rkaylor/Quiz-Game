a12bc76857116665973ca501fe3b1c3fb737d726
Robert Kaylor <rkaylor23@gmail.com>
2020-07-28 20:56:42 -0400

    New questions, end of else loops now bring you to the highscore page.

--- assets/java.js
+++ assets/java.js
@@ -14,16 +14,16 @@ var interval = setInterval(function(){
 }, 1000);
 //Arrays
 var questionsAnswers = [
-    { question: "Whats the capital of Texas?", answer: 3 },
-    { question: "What color is the sky?", answer: 1 },
-    { question: "What shape is a warning sign?", answer: 3 },
-    { question: "What color is the ocean?", answer: 4 },
+    { question: "Who made the film 'The Birds?", answer: 3 },
+    { question: "What was the name of the prequel film for 'Alien'?", answer: 1 },
+    { question: "What Film won best picture in 2020?", answer: 3 },
+    { question: "Who was the lead actor of Planet of the Apes (1968)?", answer: 4 },
   ];
   var answers = [
-    { "1": "San Diego", "2": "Orlando", "3": "Houston", "4": "New York" },
-    { "1": "Blue", "2": "Orange", "3": "Yellow", "4": "Black" },
-    { "1": "Octagon", "2": "Circle", "3": "Diamond", "4": "Square" },
-    { "1": "Green", "2": "Violet", "3": "Yellow", "4": "Blue" },
+    { "1": "Martin Scorsese", "2": "Orson Welles", "3": "Alfred Hitchcock", "4": "Stanley Kubrick" },
+    { "1": "Prometheus", "2": "Alien Resurrection", "3": "Alien vs Predator", "4": "Alien:Origins" },
+    { "1": "Once Upon a Time in Hollywood", "2": "Joker", "3": "Parasite", "4": "1917" },
+    { "1": "Andy Serkis", "2": "Mark Wahlberg", "3": "Roddy McDowall", "4": "Charlton Heston" },
   ];
   console.log(
     questionsAnswers.length,
@@ -56,6 +56,8 @@ var questionsAnswers = [
       $("#userScoreHTML").text(userScore);
       } else {
           $("#userScoreHTML").text(userScore);
+          localStorage.setItem("userScoreFinal",userScore);
+          window.location.href = "highscore.html";
       }
     } else {
              index++;
@@ -67,7 +69,11 @@ var questionsAnswers = [
              $(".answers4").text(answers[index][4]);
              } else {
                  $("#userScoreHTML").text(userScore);
-             }
-          }
+                  localStorage.setItem("userScoreFinal",userScore)
+          window.location.href = "highscore.html";
+             }//Trying to bring up ending Arguments 
+             
+         
+            }
   });
   console.log(questionsAnswers[1].question, questionsAnswers[0].answer);
